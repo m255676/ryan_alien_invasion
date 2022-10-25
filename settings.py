b5781780d4920e5c7209761b5e5e1ca0484038e4
m255676 <m255676@usna.edu>
2022-10-25 08:02:28 -0400

    Update settings.py
    reconfigure width and height so game is visible on my computer screen

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
     def __init__(self):
         """Initialize the gam'es settings"""
         # Screen Settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 600
+        self.screen_height = 400
         self.bg_color = (230,230,230)
